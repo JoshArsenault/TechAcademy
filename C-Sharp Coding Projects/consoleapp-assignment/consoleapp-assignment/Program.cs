f3e186fff8a90bee5956da62eafdda801f29b78c
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-13 16:40:48 -0300

    Added part one

---
+++ C-Sharp Coding Projects/consoleapp-assignment/consoleapp-assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace consoleapp_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define actors and actresses
+            string[] actorNames = { "Bob Hope", "John Wick", "Johnny Depp", "Spider Man", "Scarlette Johanson" };
+
+            //intro & get funny
+            Console.WriteLine("Funny name generator");
+            Console.WriteLine("Add some text to be added to an actor name");
+            string theFunny = Console.ReadLine();
+
+            //concatenate funny to actorNames
+            for (int i = 0; i < actorNames.Length; i++)
+            {
+                actorNames[i] = actorNames[i] + " " + theFunny;
+                Console.WriteLine(actorNames[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
