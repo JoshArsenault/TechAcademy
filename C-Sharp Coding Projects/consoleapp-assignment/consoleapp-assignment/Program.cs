9d893624a6070f6543251f0dbdaabc8cfe9467f4
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-13 18:12:41 -0300

    Added rest of assignment

--- C-Sharp Coding Projects/consoleapp-assignment/consoleapp-assignment/Program.cs
+++ C-Sharp Coding Projects/consoleapp-assignment/consoleapp-assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace consoleapp_assignment
 {
@@ -6,19 +7,92 @@ namespace consoleapp_assignment
     {
         static void Main(string[] args)
         {
-            //define actors and actresses
-            string[] actorNames = { "Bob Hope", "John Wick", "Johnny Depp", "Spider Man", "Scarlette Johanson" };
+            //part 1
 
-            //intro & get funny
-            Console.WriteLine("Funny name generator");
-            Console.WriteLine("Add some text to be added to an actor name");
-            string theFunny = Console.ReadLine();
+            ////define actors and actresses
+            //string[] actorNames = { "Bob Hope", "John Wick", "Johnny Depp", "Spider Man", "Scarlette Johanson" };
 
-            //concatenate funny to actorNames
-            for (int i = 0; i < actorNames.Length; i++)
+            ////intro & get funny
+            //Console.WriteLine("Funny name generator");
+            //Console.WriteLine("Add some text to be added to an actor name");
+            //string theFunny = Console.ReadLine();
+
+            ////concatenate funny to actorNames
+            //for (int i = 0; i < actorNames.Length; i++)
+            //{
+            //    actorNames[i] = actorNames[i] + " " + theFunny;
+            //    Console.WriteLine(actorNames[i]);
+            //}
+            //Console.ReadLine();
+            //Console.Clear();
+
+            //part 2
+
+            //for (int k = 0; k != 1;)      // add k++ to fix infinite loop
+            //{
+            //    Console.WriteLine("This is an infinite loop");
+            //}
+
+            //part 3
+
+            //for (int j = 0; j < actorNames.Length; j++)
+            //{
+            //    Console.WriteLine("For each name in an array, this text will show. Except the last one");
+            //}
+
+            //for (int j = 0; j <= actorNames.Length; j++)
+            //{
+            //    Console.WriteLine("For each name in an array, this text will show. Including the last one");
+            //}
+
+            //part 4
+
+            //List<string> movieTitles = new List<string>();
+            //movieTitles.Add("The Avengers: Endgame");
+            //movieTitles.Add("Mrs. Doubtfire");
+            //movieTitles.Add("Fast and Furious");
+            //movieTitles.Add("Sonic the Hedgehog");
+            //movieTitles.Add("A Quiet Place");
+            //movieTitles.Add("The Eternals");
+
+            //Console.WriteLine("Search a movie to check our listings");
+            //string searchQuery = Console.ReadLine();
+
+            //foreach (string title in movieTitles)
+            //{
+            //    if (searchQuery.Contains(title))
+            //    {
+            //        Console.WriteLine("We have this title and it has an index of: " + movieTitles.IndexOf(searchQuery));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("We don't have this title at the time, sorry!");
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //part 5
+
+            List<string> tvshows = new List<string>();
+            tvshows.Add("WandaVision");
+            tvshows.Add("Captain America and the Winter Soldier");
+            tvshows.Add("Loki");
+            tvshows.Add("What If");
+            tvshows.Add("WandaVision");
+
+            Console.WriteLine("Enter a tv show to check our current listings:");
+            string searchQuery = Console.ReadLine();
+
+            foreach (string tvshow in tvshows)
             {
-                actorNames[i] = actorNames[i] + " " + theFunny;
-                Console.WriteLine(actorNames[i]);
+                if (searchQuery.Contains(tvshow))
+                {
+                    Console.WriteLine("We have this title and it has an index of: " + tvshows.IndexOf(searchQuery));
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, we don't yet have this title.");
+                }
             }
             Console.ReadLine();
         }
