b2c9e525cbf56550945ebf7a0d3389b253fa6e13
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-20 17:37:56 -0300

    Added deck constructor

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Program.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TwentyOne
 {
@@ -6,7 +10,14 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
     }
 }
