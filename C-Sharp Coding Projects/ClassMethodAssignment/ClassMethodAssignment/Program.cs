10aa387dfabaf585983ef72ed1811588abc6b5bd
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 20:15:20 -0300

    Started ClassMethodAssignment

---
+++ C-Sharp Coding Projects/ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
