2966d0e1bbd05d73b3f272ce84498fe853a49345
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 17:56:13 -0300

    Added method 1 and 2

---
+++ C-Sharp Coding Projects/ClassesAndMethods/ClassesAndMethodsPartOne/ClassesAndMethodsPartOne/SimpleMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ClassesAndMethodsPartOne
+{
+    public class SimpleMath
+    {
+        //adding "integer" types
+        public int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        //adding "double" types
+        public double AddNumbers(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
