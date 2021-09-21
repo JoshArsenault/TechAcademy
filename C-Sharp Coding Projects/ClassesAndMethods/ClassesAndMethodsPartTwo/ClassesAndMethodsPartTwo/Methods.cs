cb9bcf8678b3d77f601962aacd88b60cc7961742
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 18:59:12 -0300

    Added optional number pathing

---
+++ C-Sharp Coding Projects/ClassesAndMethods/ClassesAndMethodsPartTwo/ClassesAndMethodsPartTwo/Methods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ClassesAndMethodsPartTwo
+{
+    public class Methods
+    {
+        public int MultiplyNumbers(int num1, int num2 = 1) //default = 1
+        {
+            return num1 * num2;
+        }
+    }
+}
