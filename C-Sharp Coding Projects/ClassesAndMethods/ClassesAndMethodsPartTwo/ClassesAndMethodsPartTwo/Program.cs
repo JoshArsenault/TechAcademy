b2d93b525d1531f49798df6c6a721b69093ca6ad
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 18:05:48 -0300

    Started Part 2

---
+++ C-Sharp Coding Projects/ClassesAndMethods/ClassesAndMethodsPartTwo/ClassesAndMethodsPartTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesAndMethodsPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
