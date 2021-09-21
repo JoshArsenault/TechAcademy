c62795f5cc525c74982b15cf4def0788f777cc20
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 17:31:09 -0300

    Start point

---
+++ C-Sharp Coding Projects/ClassesAndMethods/ClassesAndMethodsPartOne/ClassesAndMethodsPartOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesAndMethodsPartOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
