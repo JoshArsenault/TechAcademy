18f9c1a13681c454fbe0223656dac9ebdd9f2f97
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 17:00:14 -0300

    Added basic math methods

---
+++ C-Sharp Coding Projects/MethodAssignment/MethodAssignment/Methods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MethodAssignment
+{
+    public class Methods
+    {
+        public int AddTwoNumbers(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int DivideTwoNumbers(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        public int MultiplyTwoNumbers(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
