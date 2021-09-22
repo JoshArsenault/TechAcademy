349622276f3c2e50e1b1ab208f1d0764893738c1
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-21 21:09:55 -0300

    Added methods

---
+++ C-Sharp Coding Projects/ClassMethodAssignment/ClassMethodAssignment/MultiplyOrDivide.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ClassMethodAssignment
+{
+    public class MultiplyOrDivide
+    {
+        public void DivideByTwo(int num1, out int result)
+        {
+            result = num1 / 2;
+        }
+
+        public int MultiplyByTwo(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        //this is an overload method for decimal nums
+        public double MultiplyByTwo(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double SquareRoot()
+        {
+            return Math.Sqrt(10);
+        }
+    }
+}
