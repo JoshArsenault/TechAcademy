41e73a198074be0d9aa7e07b5060db24f028ef3c
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-27 20:57:25 -0300

    Added game inheritance

---
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TwentyOne
+{
+    public class TwentyOneGame : Game
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
