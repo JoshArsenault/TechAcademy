c26350ceb25fae79f966c0ffee8d8a47f8228e7e
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-27 23:05:49 -0300

    Started interfaces assignment

---
+++ C-Sharp Coding Projects/InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
