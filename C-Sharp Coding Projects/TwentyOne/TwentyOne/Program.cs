b907c0242e906ecbe00c71b746440ca1f62a6766
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-27 22:38:20 -0300

    Added abstract classes

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Program.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Program.cs
@@ -11,25 +11,19 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jesse", "Bill", "Joe" };
+            game.Players = new List<string>() { "Jesse", "Bob", "Frank" };
             game.ListPlayers();
-            game.Play();
             Console.ReadLine();
 
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
-
-
-
-            
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
     }
 }
