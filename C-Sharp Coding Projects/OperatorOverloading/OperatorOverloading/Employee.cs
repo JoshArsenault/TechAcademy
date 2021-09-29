b6ed8a5ec047a233adc3b8308f6faefeafc86446
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-09-29 07:05:17 -0300

    Operator overloading

---
+++ C-Sharp Coding Projects/OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    public class Employee
+    {
+        //properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (!emp1.Id.Equals(emp2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- C-Sharp Coding Projects/OperatorOverloading/OperatorOverloading/Program.cs
+++ C-Sharp Coding Projects/OperatorOverloading/OperatorOverloading/Program.cs
@@ -6,7 +6,13 @@ namespace OperatorOverloading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create employees
+            Employee employee1 = new Employee() { Id = 1, FirstName = "Josh", LastName = "Arsenault" };
+            Employee employee2 = new Employee() { Id = 2, FirstName = "Frodo", LastName = "Baggins" };
+
+            //display result
+            Console.WriteLine(employee1.Id == employee2.Id);
+            Console.ReadLine();
         }
     }
 }
