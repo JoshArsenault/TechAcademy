537623df8ad903e6983a78d07d4528bb51dc8a25
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-04 13:45:46 -0300

    Updated enums

--- C-Sharp Coding Projects/EnumsAssignment/EnumsAssignment/Program.cs
+++ C-Sharp Coding Projects/EnumsAssignment/EnumsAssignment/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EnumsAssignment
 {
     class Program
     {
+        public enum DaysInWeek {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the current day");
+            string input = Console.ReadLine();
+
+            DaysInWeek day = (DaysInWeek)Enum.Parse(typeof(DaysInWeek), input);
+
+            Console.WriteLine("The current day is " + day);
+
+            Console.ReadLine();
         }
     }
 }
