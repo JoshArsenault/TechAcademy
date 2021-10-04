eb46af9dc92dc71d43ea19258d194472bf6576f9
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-04 07:42:09 -0300

    Added Generics Assignment

---
+++ C-Sharp Coding Projects/GenericsAssignment/GenericsAssignment/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsAssignment
+{
+    public class Employee<T>
+    {
+        public string Name { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ C-Sharp Coding Projects/GenericsAssignment/GenericsAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first employee
+            Employee<string> EmployeeOne = new Employee<string>();
+            EmployeeOne.Things.Add("Goldfish");
+            EmployeeOne.Things.Add("Bucket of buckets");
+            EmployeeOne.Things.Add("Gum from under his seat");
+
+            //second employee
+            Employee<int> EmployeeTwo = new Employee<int>();
+            EmployeeTwo.Things.Add(15);
+            EmployeeTwo.Things.Add(20);
+            EmployeeTwo.Things.Add(30);
+
+            //loop
+            foreach (string thingStr in EmployeeOne.Things)
+            {
+                foreach (int thingInt in EmployeeTwo.Things)
+                {
+                    Console.WriteLine(thingStr + thingInt);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
