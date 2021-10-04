37599cb4bbb748bb209798f3c12370831d9d3933
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-04 15:18:16 -0300

    Created struct

--- C-Sharp Coding Projects/StructsAssignment/StructsAssignment/Program.cs
+++ C-Sharp Coding Projects/StructsAssignment/StructsAssignment/Program.cs
@@ -4,9 +4,19 @@ namespace StructsAssignment
 {
     class Program
     {
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Number num1 = new Number();
+            num1.Amount = 15.95m;
+
+            Console.WriteLine(num1.Amount);
+
+            Console.ReadLine();
         }
     }
 }
