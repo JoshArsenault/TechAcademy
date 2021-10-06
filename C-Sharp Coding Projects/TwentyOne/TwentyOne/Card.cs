ec4667f09d1fdaba4e357c96d505a013cc732216
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-06 09:31:14 -0300

    Added card properties

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Card.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Card.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TwentyOne
 {
     public class Card
     {
-        public Card()
+        // all possible suits
+        public enum Suit
         {
+            Hearts,
+            Diamonds,
+            Spades,
+            Clubs
+        }
+
+        // all possible faces
+        public enum Face
+        {
+            One,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace 
         }
     }
 }
