de12b4f7907b879b2314bcc19184f90b60024a53
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-06 09:38:43 -0300

    Update Game.cs

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Game.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Game.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TwentyOne
 {
-    public class Game
+    public abstract class Game
     {
-        public Game()
+        // start game method
+        public abstract void Play();
+
+        // display all players in game
+        public virtual void ListPlayers()
         {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player);
+            }
         }
+
+        // game properties
+        public List<Player> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
     }
 }
