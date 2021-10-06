d2ab170ec682536a493f90d3cda67df2affaaa1d
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-06 09:35:49 -0300

    Added player properties and behaviors

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Player.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Player.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace TwentyOne
 {
     public class Player
     {
-        public Player()
+        // add player to game
+        public static Game operator+ (Game game, Player player)
         {
+            game.Players.Add(player);
+            return game;
         }
+
+        // remove player from game
+        public static Game operator- (Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+
+        //player properties
+        public List<Card> Hand { get; set; }
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public bool isActivelyPlaying { get; set; }
     }
 }
