c9fa5cf4fe4a32058d41e8fa458f8c3b7d802d34
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-07 14:54:30 -0300

    Added logging to text file

---
+++ C-Sharp Coding Projects/FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get number
+            Console.WriteLine("Please enter a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //write input to text file
+            using (StreamWriter file = new StreamWriter(@"/Users/joshcummings/Documents/Github/TechAcademy/C-Sharp Coding Projects/FileIOAssignment/FileIOAssignment/log.txt"))
+            {
+                file.WriteLine(userNumber);
+            };
+
+            //outro
+            Console.WriteLine(userNumber + " has been logged.");
+            Console.ReadLine();
+        }
+    }
+}
