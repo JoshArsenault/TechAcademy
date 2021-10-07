c6f0ee3245a7937654be53c4e00a2008a698790d
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-07 14:45:54 -0300

    Added text file logging

--- C-Sharp Coding Projects/TwentyOne/TwentyOne/Dealer.cs
+++ C-Sharp Coding Projects/TwentyOne/TwentyOne/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -18,7 +19,12 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"/Users/joshcummings/Documents/Github/TechAcademy/C-Sharp Coding Projects/TwentyOne/TwentyOne/log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }
