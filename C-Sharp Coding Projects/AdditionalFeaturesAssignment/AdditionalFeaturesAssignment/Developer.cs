c62a171b51f95a579e629efb62b64cd64be4f545
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-08 12:20:56 -0300

    Added AdditionalFeaturesAssignment

---
+++ C-Sharp Coding Projects/AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Developer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class Developer
+    {
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //add developer construct1
+        public Developer(string firstName)
+        {
+            this.FirstName = firstName;
+        }
+
+        //add developer construct2
+        public Developer(string firstName, string LastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = LastName;
+        }
+    }
+}
