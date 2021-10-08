bf9c7ffa65ffd130a7b7a0478813b83224d7516d
Josh <90134710+JoshArsenault@users.noreply.github.com>
2021-10-08 16:26:06 -0300

    Added ExceptionHandingAssignment

---
+++ C-Sharp Coding Projects/ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            bool isValid = false; //default
+            DateTime currentDate = DateTime.Now;
+
+            //display current date
+            Console.WriteLine("Current date: " + currentDate.ToString("MMMM dd yyyy"));
+
+            //container
+            while (isValid == false)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter your age");
+                    try
+                    {
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("You were born: " + currentDate.AddYears(-age).ToString("MMMM dd yyyy"));
+                        Console.ReadLine();
+                        isValid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Something went wrong");
+                        Console.WriteLine("Please enter your age");
+                        int age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("You were born: " + currentDate.AddYears(-age).ToString("MMMM dd yyyy"));
+                        Console.ReadLine();
+                        isValid = true;
+                        Console.ReadLine();
+                        return;
+                    }
+                    
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid age");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+
+            //delay
+            Console.ReadLine();
+        }
+    }
+}
